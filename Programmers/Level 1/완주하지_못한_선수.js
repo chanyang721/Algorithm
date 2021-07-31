5b45d5b9a9020ba1d15fa59c4da973147a01e6a4
chanyang217 <chanyang721@gmail.com>
2021-07-31 22:49:10 +0900

    21.07.31

--- Programmers/Level 1/완주하지_못한_선수.js
+++ Programmers/Level 1/완주하지_못한_선수.js
@@ -1,3 +1,16 @@
+// 효율성 오답 // 
+function solution(participant, completion) {
+    
+    for (let i = 0; i < completion.length; i++) {
+        if ( participant.includes(completion[i]) ) {
+            let resultIdx = participant.findIndex(el => el === completion[i])
+            participant.splice(resultIdx, 1);
+        }
+    }
+    return participant[0];
+}
+
+// 정답 // 
 function solution(participant, completion) {
     let result = '';
     participant.sort()
