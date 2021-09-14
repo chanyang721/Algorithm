acbe33e79bc369c575fe42dc8ff88332841e471a
chanyang217 <chanyang721@gmail.com>
2021-09-14 22:54:13 +0900

    21.09.14

--- Programmers/Level 2/튜플.js
+++ Programmers/Level 2/튜플.js
@@ -1,10 +1,17 @@
+// O(n) // 26.34ms //
 function solution(s) {
-    // 순서있는 열거, 순서를 따르는 요소의 모음을 튜플이라 한다.
-    // 중복 요소 가능
-    // 순서가 다르면 다른 튜플이다.
-    // 중복된 요소가 없는 튜플이 주어진다.
-    // 많이 나온 순서대로 앞으로 정렬
-    let base = s.replace(/{/gi, "").replace(/}/gi, "").replace(/,/gi, " ").split(" ");
+    let base = JSON.parse(s.replace(/{/g,'[').replace(/}/g,']'));
+    
+    base = base.sort((a, b) => a.length - b.length).flat();
+
+    let result = [ ...new Set(base) ]
+    
+    return result;
+}
+
+// O(n^2) // 71.07ms //
+function solution(s) {
+    let base = s.replace(/{/g, "").replace(/}/g, "").replace(/,/gi, " ").split(" ");
     let database = [];
 
     for (let i = 0; i < base.length; i++) {
