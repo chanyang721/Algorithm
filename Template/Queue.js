2aebd94ed863818ab9e3d0b8801359bec9e7c5ed
chanyang <chanyang721@gmail.com>
2022-08-16 18:09:34 +0900

    [ SET ] data structure file

--- Template/Queue.js
+++ Template/Queue.js
@@ -1,3 +1,6 @@
+// Queue 란 ?
+// 
+
 const Q = [];
 const enQ = (n) => Q.push(n);
 const deQ = () => Q.shift();
@@ -6,4 +9,4 @@ const isVisited = new Array(matrix.length).fill(false);
 
 for (let i = 0; i < maxNum + 1; i++) {
 	  matrix.push(new Array(maxNum + 1).fill(0));
-	}
+}
