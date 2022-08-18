bc1d0b453adf58ac563013f3bea44651fa1a04f1
chanyang <chanyang721@gmail.com>
2022-08-18 10:51:10 +0900

    [ DataStructure ] LinkedList

--- DataStructures/linked-list/linkedList.js
+++ DataStructures/linked-list/linkedList.js
@@ -1,9 +1,86 @@
+class LinkedListNode {
+    constructor (value, next = null, previous = null) {
+        this.value = value;
+        this.next = next;
+        this.previous = previous;
+    }
+}
+
 class LinkedList {
 
-    constructor() {
-
+    constructor(compareFunction) {
+        this.head = null;
+        this.tail = null;
     }
 
 
+    prepend(value) {
+        const newNode = new LinkedListNode(value, this.head);
+
+        if (this.head) {
+            this.head.previous = newNode;
+        }
+        this.head = newNode;
+
+        if (!this.tail) {
+            this.tail = newNode;
+        }
+
+        return this;
+    }
+
+    append(value) {
+        const newNode = new LinkedListNode(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+
+            return this
+        }
+
+        this.tail.next = newNode; 
+        this.tail = newNode;
+
+        return this
+    }
+
+    insert (value, rawIndex) {
+        const idx = rawIndex < 0 ? 0 : rawIndex;
+
+        if (index === 0) {
+            this.prepend(value)
+        }
+        else {
+            let count = 1;
+            let currentNode = new LinkedListNode(value);
+
+            while (currentNode) {
+                if (count === index) break;
+
+                currentNode = currentNode.next;
+                count += 1;
+            }
+
+            if (currnetNode) {
+                newNode.next = currentNode.next;
+                currnetNode.next = newNode;
+            }
+            else {
+                if (this.tail) {
+                    this.tail.next = newNode;
+                    this.tail = newNode;
+                }
+                else {
+                    this.head = newNode;
+                    this.tail = newNode;
+                }
+            }
+        }
+
+        return this
+    }
+
     
+
 }
