fc322447a4c518b1f6b69de6280ff659976b147b
Chanyang <chanyang721@gmail.com>
2025-05-03 16:44:53 +0900

    typedof, #define

---
+++ cpp/basic.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.141592; // #define 으로 매크로 상수 정의
+
+typedef int INT; // typedof 으로 타입 이름을 별칭으로 정의 가능
+
+INT a = 10;
+
+int main()
+{
+    INT a = 10;
+    cout << a << endl;
+    return 0;
+}
