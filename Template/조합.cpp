e30e5faf0850a4f714ed7315d60c999f1c681ccd
Chanyang <chanyang721@gmail.com>
2025-05-12 14:30:51 +0900

    조합 반복문

--- Template/조합.cpp
+++ Template/조합.cpp
@@ -33,7 +33,19 @@ void combi(int start, vector<int> &b)
 }
 int main()
 {
-    vector<int> b;
-    combi(-1, b);
+    // vector<int> b;
+    // combi(-1, b);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                cout << i << " " << j << " " << k << "\n";
+            }
+        }
+    }
+
     return 0;
 }
