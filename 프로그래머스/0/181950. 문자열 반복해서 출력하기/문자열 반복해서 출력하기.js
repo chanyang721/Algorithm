cd5cf26f0f27513a965e1f0172e310d0eb810ada
Chanyang Lee <chanyang721@gmail.com>
2025-05-13 15:35:22 +0900

    문자열 반복해서 출력하기.js

--- 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.js
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.js
@@ -11,5 +11,13 @@ rl.on('line', function (line) {
 }).on('close', function () {
     str = input[0];
     n = Number(input[1]);
+    // repest() 사용
     console.log(str.repeat(n))
+
+    // for문  사용
+    let result = "";
+    for (let i = 0; i < n; i++) {
+        result += str;
+    }
+    console.log(result)
 });
