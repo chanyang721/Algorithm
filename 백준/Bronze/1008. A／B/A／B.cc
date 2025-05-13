e56f3addb44a67e50dc8b17e6961faa645d4042b
Chanyang Lee <chanyang721@gmail.com>
2025-05-13 12:50:39 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double A, B;
+    cin >> A >> B;
+    double result = A / B;
+    cout.precision(10);
+    cout << result;
+    return 0;  
+}
