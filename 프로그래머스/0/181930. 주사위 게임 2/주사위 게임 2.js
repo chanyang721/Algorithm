801310edd28636fbd73c6c6163202c65b6694c0a
Chanyang Lee <chanyang721@gmail.com>
2025-05-15 16:34:14 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,13 +1,20 @@
 function solution(a, b, c) {
-    var answer = 0;
-    const uniqueArr = [...new Set([a, b, c])]
+    var answer = 1;
+    const inputArr = [a, b, c]
+    const uniqueArr = [...new Set(inputArr)]
+            
+    const col = (arr, n) => {
+        const [a,b,c] = arr;
+        for (let i = 1; i <= n; i++) {
+            answer *= Math.pow(a, i) + Math.pow(b, i) + Math.pow(c, i)
+        }
+        return answer
+    }
     
-    if(uniqueArr.length === 3) {
-        answer = a + b + c
-    } else if (uniqueArr.length === 2) {
-        answer = (a + b + c) * (a**2 + b**2 + c**2)
-    } else if (uniqueArr.length === 1) {
-        answer = (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+    switch (uniqueArr.length) {
+        case 1: return col(inputArr, 3)
+        case 2: return col(inputArr, 2)
+        case 3: return col(inputArr, 1)
     }
     
     return answer;
