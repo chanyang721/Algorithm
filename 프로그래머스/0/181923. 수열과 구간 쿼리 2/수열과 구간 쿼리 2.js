6eab00e9f69adea31eec63f35ce1debf20e905ed
Chanyang Lee <chanyang721@gmail.com>
2025-05-18 10:30:13 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 28.53 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,11 +1,13 @@
 function solution(arr, queries) {
     var answer = [];
     let min = 0;
+    const max = 1000000;
+    
     for ([s,e,k] of queries) {
         min = arr.slice(s, e+1)
             .filter(el => el > k)
-            .reduce((min, num) => Math.min(min, num), Infinity);
-        if (min === Infinity) min = -1;
+            .reduce((min, num) => Math.min(min, num), max);
+        if (min === max) min = -1;
         answer.push(min)
     }
     return answer;
