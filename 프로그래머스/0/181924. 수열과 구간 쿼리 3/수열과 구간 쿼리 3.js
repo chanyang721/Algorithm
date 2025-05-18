80a97087b59bc7e0bd71f7aa4fbd703729d649ed
Chanyang Lee <chanyang721@gmail.com>
2025-05-18 10:05:55 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.88 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,11 +1,17 @@
 function solution(arr, queries) {
     var answer = [];
-    for (let k = 0; k < queries.length; k++) {
-        const [i, j]= queries[k];
-        const I = arr[i];
-        const J = arr[j];
-        arr[i] = J;
-        arr[j] = I;
+    // for (let k = 0; k < queries.length; k++) {
+    //     const [i, j]= queries[k];
+    //     const I = arr[i];
+    //     const J = arr[j];
+    //     arr[i] = J;
+    //     arr[j] = I;
+    // }
+    const swap = ([i, j]) => {
+        [[arr[i], arr[j]]] = [[arr[j], arr[i]]]
     }
+    
+    for(let k of queries) swap(k);
+    
     return arr;
 }
