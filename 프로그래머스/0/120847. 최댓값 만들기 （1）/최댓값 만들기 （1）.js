b27b67a5fa69caac7d3ea733742a1818ed2d5944
Chanyang Lee <chanyang721@gmail.com>
2025-06-01 06:51:44 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -2,14 +2,19 @@ function solution(numbers) {
     // numbers max (el1 * el2)
     // 1. for i 0 -> len
     //      for j = i + 1 -> len
-    var answer = numbers[0];
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            let num = numbers[i] * numbers[j];
-            if (num > answer) {
-                answer = num;
-            }
-        }
-    }
+    // var answer = numbers[0];
+    // for (let i = 0; i < numbers.length; i++) {
+    //     for (let j = i + 1; j < numbers.length; j++) {
+    //         let num = numbers[i] * numbers[j];
+    //         if (num > answer) {
+    //             answer = num;
+    //         }
+    //     }
+    // }
+    
+    // 2. sort (b-a), 0 * 1
+    var answer = 0;
+    numbers.sort((a, b) => b - a)
+    answer = numbers[0] * numbers[1]
     return answer;
 }
