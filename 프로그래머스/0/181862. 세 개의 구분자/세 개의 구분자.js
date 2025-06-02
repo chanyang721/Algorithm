37eb27406de89fb1026ce32abfab38fcdf07e2ec
Chanyang Lee <chanyang721@gmail.com>
2025-06-02 14:14:16 +0900

    [level 0] Title: 세 개의 구분자, Time: 33.94 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,12 +1,18 @@
 function solution(myStr) {
     // divider = a,b,c
     // [] ? return ["EMPTY"]
-    var answer = []
-    const isEmpty = ["EMPTY"]
-    answer = myStr.split("a")
-        .map(el => el.split("b")).flat()
-        .map(el => el.split("c")).flat()
-        .filter(el => el !== "")
+    // 1. 
+    // var answer = []
+    // const isEmpty = ["EMPTY"]
+    // answer = myStr.split("a")
+    //     .map(el => el.split("b")).flat()
+    //     .map(el => el.split("c")).flat()
+    //     .filter(el => el !== "")
+    // return answer.length === 0 ? isEmpty : answer
     
-    return answer.length === 0 ? isEmpty : answer
+    // 2. 정규식
+     const result = myStr.split(/[abc]/).filter(str => str !== "");
+    return result.length ? result : ["EMPTY"];
+    
+    
 }
