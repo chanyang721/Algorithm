c569a7a6b1fb4b3325939705ddd50941edac3f25
Chanyang Lee <chanyang721@gmail.com>
2025-06-11 09:19:48 +0900

    [level 0] Title: 배열의 유사도, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -5,13 +5,14 @@ function solution(s1, s2) {
     //           s1[i] === s2[j] ? answer++
     //      2. if s2.includes[s1[i]] ? answer++
     //      3. if s2.indexOf[s1[i]] !== -1 answer++
-    var answer = 0;
+    //      4. [...s1, ...s2].length - [...new Set([...s1, ...s2])].length
+//     var answer = 0;
     
-    for (let i = 0; i < s1.length; i++) {
-        if (s2.includes(s1[i])) {
-            answer++ 
-        }
-    }
+//     // for (let i = 0; i < s1.length; i++) {
+//     //     if (s2.includes(s1[i])) {
+//     //         answer++ 
+//     //     }
+//     // }
 
-    return answer;
+    return [...s1, ...s2].length - [...new Set([...s1, ...s2])].length
 }
