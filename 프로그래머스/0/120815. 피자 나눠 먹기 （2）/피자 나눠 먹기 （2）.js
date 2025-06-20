50cd88942e2b8153b2ec0454bbfcc40b8d4c0f36
Chanyang Lee <chanyang721@gmail.com>
2025-06-20 12:18:24 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -0,0 +1,14 @@
+function gcd(a, b) {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return a;
+}
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+
+function solution(n) {
+  return lcm(n, 6) / 6;
+}
